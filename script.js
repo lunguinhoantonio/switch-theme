6c6561f9562351f455344178cf5f828f4e3bfa5c
Antonio Lunguinho <antoniolunguinho007@gmail.com>
2024-06-29 15:37:41 -0300

    yipeee bwomp

--- script.js
+++ script.js
@@ -5,17 +5,22 @@ function switchTheme() {
     const titleButton = document.querySelector('.switch-theme');
     const textButton = document.querySelector('.switch-theme');
     const h1Name = document.querySelector('.switch-theme-name');
-    const h1Emoji = document.querySelector('.emoji')
-
+    const h1Emoji = document.querySelector('.emoji');
+    const sound = new Audio();
+    sound.src = 'assets/bwomp.m4a';
+    
     if(body.classList.contains('dark')){
         titleButton.setAttribute('title', 'Clique para trocar tema para claro (tristeza)');
         textButton.innerHTML = 'Trocar para tema claro';
         h1Name.innerHTML = 'Tema Escuro :D';
         h1Emoji.innerHTML = '😎😍🥰😘🤩😝😉😊😃';
+        sound.src = 'assets/yipeee.m4a';
     } else {
         titleButton.setAttribute('title', 'Clique para trocar tema para escuro (felicidade)');
         textButton.innerHTML = 'Trocar para tema escuro';
         h1Name.innerHTML = 'Tema Claro :(';
         h1Emoji.innerHTML = '😥😔😭😩😖😰🤕🤮🤢';
+        sound.src = 'assets/bwomp.m4a';
     }
+    sound.play();
 }
