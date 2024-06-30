2dacb59898f0f8cf1ce4eb08b5db633bf5b7e1b2
Antonio Lunguinho <antoniolunguinho007@gmail.com>
2024-06-30 00:38:26 -0300

    :( :D (incompleto)

--- script.js
+++ script.js
@@ -7,20 +7,40 @@ function switchTheme() {
     const h1Name = document.querySelector('.switch-theme-name');
     const h1Emoji = document.querySelector('.emoji');
     const sound = new Audio();
-    sound.src = 'assets/bwomp.m4a';
+    sound.src = './assets/bwomp.m4a';
     
     if(body.classList.contains('dark')){
         titleButton.setAttribute('title', 'Clique para trocar tema para claro (tristeza)');
         textButton.innerHTML = 'Trocar para tema claro';
         h1Name.innerHTML = 'Tema Escuro :D';
         h1Emoji.innerHTML = '😎😍🥰😘🤩😝😉😊😃';
-        sound.src = 'assets/yipeee.m4a';
+        sound.src = './assets/yipeee.m4a';
     } else {
         titleButton.setAttribute('title', 'Clique para trocar tema para escuro (felicidade)');
         textButton.innerHTML = 'Trocar para tema escuro';
         h1Name.innerHTML = 'Tema Claro :(';
         h1Emoji.innerHTML = '😥😔😭😩😖😰🤕🤮🤢';
-        sound.src = 'assets/bwomp.m4a';
+        sound.src = './assets/bwomp.m4a';
     }
     sound.play();
+}
+
+function playYipeee() {
+    const sound = new Audio();
+    sound.src = './assets/yipeee.m4a';
+    sound.play();
+}
+
+function playBwomp() {
+    const sound = new Audio();
+    sound.src = './assets/bwomp.m4a';
+    sound.play();
+}
+
+let easterEgg = document.querySelector('easter-egg');
+let unusualButtons = document.querySelector('.unusual-buttons');
+let i = 0;
+function click() {
+    ++i;
+    console.log(i);
 }
